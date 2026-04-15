3a3b91a75a4e3afd4ecdc3635fef766b5f887061
Kervy Tabigue <kervytabigue69@gmail.com>
2026-04-15 19:39:38 +0800

    First Commit

---
+++ accounts/refresh-token.model.ts
@@ -0,0 +1,31 @@
+import { DataTypes } from 'sequelize';
+export default function model(sequelize: any) {
+    const attributes = {
+        token: { type: DataTypes.STRING },
+        expires: { type: DataTypes.DATE },
+        created: {
+        type: DataTypes.DATE, allowNull: false,  defaultValue: DataTypes.NOW
+        },
+        createdByIp: { type: DataTypes.STRING },
+        revoked: { type: DataTypes.DATE },
+        revokedByIp: { type: DataTypes.STRING },
+        replacedByToken: { type: DataTypes.STRING },
+        isExpired: {
+            type: DataTypes.VIRTUAL,
+        get() {
+            return new Date() >= new Date(this.getDataValue('expires'));
+            }
+        },
+        isActive: {
+            type: DataTypes.VIRTUAL,
+        get() {
+             return !this.getDataValue('revoked') &&
+            !this.getDataValue('isExpired');
+            }
+        }
+    };
+    const options = {
+        timestamps: false
+    };
+    return sequelize.define('refreshToken', attributes, options);
+}
